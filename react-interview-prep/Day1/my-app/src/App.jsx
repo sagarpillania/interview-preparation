87a8b519be72be4ac1640ca12616285f6d84a207
Sagar Pillania <121743997+sagarpillania@users.noreply.github.com>
2026-06-16 10:02:17 +0530

    First React

---
+++ react-interview-prep/Day1/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import FilterableProductTable from "./Product"
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+function App() {
+ return <FilterableProductTable products={PRODUCTS}/>
+}
+
+export default App
