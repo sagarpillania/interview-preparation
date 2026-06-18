ac57b663799a129a38538f41ebb925c0815b2e44
Sagar Pillania <121743997+sagarpillania@users.noreply.github.com>
2026-06-18 10:24:51 +0530

    played with props

--- react-interview-prep/Day2/my-app/src/App.jsx
+++ react-interview-prep/Day2/my-app/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 // import './App.css'
 import Proopss from './Proopps';
+import Button from './Button';
+import Animal from './Animal';
+
+const animals=["Lion","Zebra","Deer","Buffalow"];
 
 function App() {
   const [count,setCount]=useState(0);
@@ -16,12 +20,19 @@ function App() {
       setCount(0);
     }
   }
+
+  function handleClick(url){
+    window.location.href=url;
+  }
+
   return (
     <>
     <p> Count is {count}</p>
     <button onClick={Incre}>Increment</button>
     <button onClick={Decre}>Decrement</button>
     <Proopss name="sagar" age={25}/>
+    <Button onClick={()=> handleClick("https://www.google.com")}/>
+    <Animal animals={animals}/>
     </>
   )
 }
---
+++ react-interview-prep/Day2/my-app/src/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({onClick})
+{
+    return(
+        <>
+            <button onClick={onClick}>Click to go to the embedded url</button>
+        </>
+    )
+}
